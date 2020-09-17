c0c72480dd717b4fd906aa9e5a671f6572133233
Nyoung_Ni <71180175+yheun03@users.noreply.github.com>
2020-09-17 20:32:29 +0900

    다음버튼 완성, 코드 간결화, 정확성 높임

--- script/gallery.js
+++ script/gallery.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     // NOTE: 선택된 페이지에 맞춰 사진 보여주는 매개함수
-    function showPhoto(inja){
+    function showCompetition(inja){
         switch (inja){
             case 1:
                 $(".photo").css({"background-image":  "none"})
@@ -67,22 +67,84 @@ $(document).ready(function(){
                 }
                 $("#poster").css({"background-image":  "url('./img/competition/4.jpg')"})
                 break;
-            // case "3":
-            //     url = "./img/competition/first.jpg";
-            //     alert(i)
-            //     $("#poster").css({"background-image": url})
-            //     break;
+            case 5:
+                $(".photo").css({"background-image":  "none"})
+                $("#poster_title").html("5")
+                $("#poster_leader").html("")
+                $("#poster_teammate").html("")
+                $("#poster_result").html("")
+                let img5 = [
+                ]
+                for(let i = 0; i <= img5.length; i++){
+                    $("#photo"+i).css({"background-image":  "url('./img/competition/5/"+i+".jpg')"})
+                }
+                $("#poster").css({"background-image":  "url('./img/competition/5.jpg')"})
+                break;
+            case 6:
+                $(".photo").css({"background-image":  "none"})
+                $("#poster_title").html("6")
+                $("#poster_leader").html("")
+                $("#poster_teammate").html("")
+                $("#poster_result").html("")
+                let img6 = [
+                    "./img/competition/4/1.jpg",
+                    "./img/competition/4/2.jpg",
+                    "./img/competition/4/3.jpg",
+                    "./img/competition/4/4.jpg",
+                    "./img/competition/4/5.jpg"
+                ]
+                for(let i = 0; i <= img6.length; i++){
+                    $("#photo"+i).css({"background-image":  "url('./img/competition/6/"+i+".jpg')"})
+                }
+                $("#poster").css({"background-image":  "url('./img/competition/6.jpg')"})
+                break;
+            case 7:
+                $(".photo").css({"background-image":  "none"})
+                $("#poster_title").html("7")
+                $("#poster_leader").html("")
+                $("#poster_teammate").html("")
+                $("#poster_result").html("")
+                let img7 = [
+                    "./img/competition/4/1.jpg",
+                    "./img/competition/4/2.jpg",
+                    "./img/competition/4/3.jpg",
+                    "./img/competition/4/4.jpg",
+                    "./img/competition/4/5.jpg"
+                ]
+                for(let i = 0; i <= img7.length; i++){
+                    $("#photo"+i).css({"background-image":  "url('./img/competition/7/"+i+".jpg')"})
+                }
+                $("#poster").css({"background-image":  "url('./img/competition/7.jpg')"})
+                break;
+            case 8:
+                $(".photo").css({"background-image":  "none"})
+                $("#poster_title").html("8")
+                $("#poster_leader").html("")
+                $("#poster_teammate").html("")
+                $("#poster_result").html("")
+                let img8 = [
+                    "./img/competition/4/1.jpg",
+                    "./img/competition/4/2.jpg",
+                    "./img/competition/4/3.jpg",
+                    "./img/competition/4/4.jpg",
+                    "./img/competition/4/5.jpg"
+                ]
+                for(let i = 0; i <= img8.length; i++){
+                    $("#photo"+i).css({"background-image":  "url('./img/competition/8/"+i+".jpg')"})
+                }
+                $("#poster").css({"background-image":  "url('./img/competition/8.jpg')"})
+                break;
         }
     }
 
     // NOTE: 대회 열람 순서
-    for(let i = 1; i<=9; i++){
-        $(".project_number:nth-child("+i+")").click(function(){
-            $(".project_number").removeClass("n_c_50").addClass("n_a_50")
-            $(this).removeClass("n_a_50").addClass("n_c_50")
-            showPhoto(i-1)
-        })
-    }
+    $(".project_number").click(function(){
+        let pagecnt1 = parseInt($(this).html()) -1;
+        console.log(pagecnt1)
+        $(".project_number").removeClass("n_c_50").addClass("n_a_50")
+        $(this).removeClass("n_a_50").addClass("n_c_50")
+        showCompetition(pagecnt1+1)
+    })
 
     // NOTE: PHOTO 갤러리
     for(let i = 1; i<=9; i++){
@@ -100,20 +162,44 @@ $(document).ready(function(){
     // NOTE: 전 페이지로 이동
     $(".project_arrow:first-child").click(function(){
         let pagecnt1 = parseInt($('#project .n_c_50').html());
-        if(pagecnt1 != 1){
+        console.log(pagecnt1)
+        if(pagecnt1 != 1 || pagecnt2 <= 5){
             $(".project_number").removeClass("n_c_50").addClass("n_a_50")
             $(".project_number:nth-child("+pagecnt1+")").removeClass("n_a_50").addClass("n_c_50")
-            showPhoto(pagecnt1 - 1)
+            showCompetition(pagecnt1 - 1)
+        }else if(pagecnt2 > 5 || pagecnt2 <= 9){
+            for(let y = 2; y <= 5; y++){
+                $("#project_h > div .project_number:nth-child("+y+")").text(y+3);
+            }
+            if(pagecnt2 <= 8+1){
+                $(".project_number").removeClass("n_c_50").addClass("n_a_50")
+                $(".project_number:nth-child("+(pagecnt2 - 4)+")").removeClass("n_a_50").addClass("n_c_50")
+                showCompetition(pagecnt2 - 1)
+            }else{
+                $(".project_arrow:last-child").css({"opacity":".4"})
+            }
+
         }
     })
 
     // NOTE: 다음 페이지로 이동
     $(".project_arrow:last-child").click(function(){
         let pagecnt2 = parseInt($('#project .n_c_50').html())+ 2;
-        if(pagecnt2 <= $(".project_number").length+1){
+        if(pagecnt2 <= 5){
             $(".project_number").removeClass("n_c_50").addClass("n_a_50")
             $(".project_number:nth-child("+pagecnt2+")").removeClass("n_a_50").addClass("n_c_50")
-            showPhoto(pagecnt2 - 1)
+            showCompetition(pagecnt2 - 1)
+        }else if(pagecnt2 > 5 || pagecnt2 <= 9){
+            for(let y = 2; y <= 5; y++){
+                $("#project_h > div .project_number:nth-child("+y+")").text(y+3);
+            }
+            if(pagecnt2 <= 8+1){
+                $(".project_number").removeClass("n_c_50").addClass("n_a_50")
+                $(".project_number:nth-child("+(pagecnt2 - 4)+")").removeClass("n_a_50").addClass("n_c_50")
+                showCompetition(pagecnt2 - 1)
+            }else{
+                $(".project_arrow:last-child").css({"opacity":".4"})
+            }
         }
     })
 
